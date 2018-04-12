4ae9734143381cb23263da21c3139e1af0b4009c
Frederic PAUT <fpaut@free.fr>
2018-04-12 14:59:52 +0200

    Add javac, javadoc, and jarsigner selection

--- s_java_select.sh
+++ s_java_select.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 PARAM=$@
-if [ -z "$PARAM" ]; then
-	CMD="update-java-alternatives -l"
-	echo "$CMD"
-	eval $CMD
-	echo "Relaunching and give the java name as first parameter"
-else
-	CMD="sudo update-java-alternatives $PARAM"
-	echo $CMD
-	eval $CMD
-fi
+CMD="sudo update-alternatives --config java"; echo "$CMD"; eval $CMD
+CMD="sudo update-alternatives --config javac"; echo "$CMD"; eval $CMD
+CMD="sudo update-alternatives --config javadoc"; echo "$CMD"; eval $CMD
+CMD="sudo update-alternatives --config jarsigner"; echo "$CMD"; eval $CMD
 
