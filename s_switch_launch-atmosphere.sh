77bf02e84acd406380970ceeb6057496195446ac
Frederic Paut <frederic.paut@diasys-technologies.com>
2021-03-31 19:01:41 +0200

    add s_switch_launch-atmosphere.sh

---
+++ s_switch_launch-atmosphere.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CMD="sudo /home/user/dev/Perso/fusee-launcher/fusee-launcher.py -w /home/user/dev/Perso/ns-atmosphere-programmer-ubuntu-64bit-v01/atmosphere-sdfiles-201222-v25/Atmosphere-SDFiles-201222-v2.5/hekate_ctcaer_5.5.0.bin
+
+echo $CMD
+eval $CMD
