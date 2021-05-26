b69a29cd335072a0e3fdd98f7dcc9eeb19bb9897
Frederic Paut <fpaut@free.fr>
2021-05-26 10:05:00 +0200

    minor update

--- s_dos2unix.sh
+++ s_dos2unix.sh
@@ -7,9 +7,11 @@ do
     if [[ -f "$file" ]]; then 
         ASCII=$(file "$file" | egrep "ASCII|Bourne-Again|UTF-8|Perl|HTML")
         if [[ "$ASCII" != "" ]]; then
-            echo -en $GREEN"$file:  "$CYAN; dos2unix "$file"; echo -n $ATTR_RESET
-            
+            dos2unix "$file"
         fi
+    else
+        echo -en $MAGENTA"$file not ASCII"; echo -n $ATTR_RESET
+        echo
     fi
 done
 cd -
@@ -17,12 +19,12 @@ echo "cd -"
 echo Convert end of line of SSH files to unix format
 cd $HOME/.ssh
 echo "cd $HOME/.ssh"
-ls | while read file
+ls -a | while read file
 do
     if [[ -f "$file" ]]; then 
         ASCII=$(file "$file" | egrep "ASCII|Bourne-Again|UTF-8|Perl|HTML")
         if [[ "$ASCII" != "" ]]; then
-            echo -en $GREEN"$file:  "$CYAN; dos2unix "$file"; echo -n $ATTR_RESET
+            dos2unix "$file"
         fi
     fi
 done
